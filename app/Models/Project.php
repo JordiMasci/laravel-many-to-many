80892e47be9ea517ef2df368efdf89c4ee884fe8
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-10-27 18:10:15 +0200

    feat: collegamento modelli technology e project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,11 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Technology;
+
 class Project extends Model
 {
     use HasFactory;
 
-    public function type(){
+    public function type()
+    {
         return $this->belongsTo(Type::class);
+    }
+
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
     }
 }
--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Project;
 class Technology extends Model
 {
     use HasFactory;
+
+    public function projects(){
+        return $this->belongsToMany(Project::class);
+    }
 }
